18f20ad27bab2934a6143732439729f3fc7f5450
dhunanyan <davit.hunanyan@me.com>
2021-10-24 05:50:11 +0200

    PORT

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
-
+import os
 app = create_app()
 
+port = os.environ.get("PORT", 5000)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=port)
